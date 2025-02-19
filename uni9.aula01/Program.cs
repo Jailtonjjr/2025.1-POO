0d7f01b36ef255170acc58f8be24bd40cc3899e0
Jailtonjjr <128049513+Jailtonjjr@users.noreply.github.com>
2025-02-19 13:50:31 +0000

    aula_03

--- uni9.aula01/Program.cs
+++ uni9.aula01/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using aula_03;
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Ola, mundo");
+
+Televisao tvSala = new Televisao(50);
+
+// tvSala.Tamanho = 32;
+Console.WriteLine($"O tamanho da tv é: {tvSala.Tamanho}");
+
+
+
+
+
+
---
+++ uni9.aula01/Televisao.cs
@@ -0,0 +1,37 @@
+namespace aula_03;
+
+class Televisao
+{  
+    // o metodo construtr possui o mesmo
+    // nome da classe. ele nao possui retorno
+    // nem mesmo o void
+    // este metodo é executado sempre que uma 
+    // instancia da classe é criada
+    // por padrao, o c# cria um metodo construtor publico vazil
+    // mas podemos criar metodos construtores com outras
+    // visibilidades e recebendo paramentros, se necessario.
+    
+    public Televisao(float tamanho)
+    {
+        Tamanho = tamanho;
+    }
+
+
+    // Get, permite que seja executado a leitura
+    // do valor atual da propriedade
+    // Set, permite que seja atribuido um
+    // valor para propriedade
+
+    // classes, propriedade e metodos possuem modificadores de acesso
+    // eles são public:visiveis a todo o projeto
+    // internal: visivel somente no namespace
+    // protected: visiveis somente na classe e nas classses que herdam
+    // private: visivel somente na classe que foram criadas
+    public float Tamanho { get; private set; }
+    public int Resolucao { get; set; }
+    public int Volume { get; set; }
+    public int Canal { get; set; }
+    public bool Estado{ get; set; }
+   
+
+}
