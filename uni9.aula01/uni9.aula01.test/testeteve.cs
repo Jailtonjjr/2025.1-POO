6ab51d38223b32471d27252643b0a2e1a5fc4e53
Jailtonjjr <128049513+Jailtonjjr@users.noreply.github.com>
2025-02-26 14:30:44 +0000

    aula prte2_

--- uni9.aula01/uni9.aula01.test/testeteve.cs
+++ uni9.aula01/uni9.aula01.test/testeteve.cs
@@ -20,3 +20,9 @@
 //     }
     
 // }
+
+
+// Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "aula-03", "aula-03\aula-03.csproj", "{6A9C91B4-AABC-E023-9697-F6622D253970}"
+// EndProject
+// Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "aula-03.Test", "aula-03.Test\aula-03.Test.csproj", "{C48BF267-76EB-B2A9-52E1-065C3E3158DA}"
+// EndProject
